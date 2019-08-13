9b56a40421bc2a5203540d55ae8355fae7869675
Randy Warrington <warrington.randy@outlook.com>
2019-08-13 19:11:17 -0400

    MVP completed

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code away!
+
+const express = require('express');
+
+const server = express();
+const postRouter = require('./posts/postRouter');
+const userRouter = require('./users/userRouter');
+const serverRouter = require('./server');
+
+
+server.use(express.json());
+
+server.use('/', serverRouter)
+server.use('/api/users', userRouter);
+server.use('/api/posts', postRouter);
+
+
+server.listen(5000, () => {
+    console.log('Server is running on port 5000...');
+});
